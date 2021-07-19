205d4ffba2cdfedd257ec53c62f918fdbff9d552
“577914237” <577914237@qq.com>
2021-07-19 18:35:19 +0800

    主页初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,24 @@
 <template>
 	<div class="home">
 		<div class="noLogin">
-			<img :src="profile.avatarUrl" />
+			<img v-if="Islogin" :src="profile.avatarUrl" />
+			<svg v-else class="icon avatarIcon" aria-hidden="true">
+				<use xlink:href="#icon-yinle"></use>
+			</svg>
 			<h2 v-if="Islogin">欢迎 {{profile.nickname}}</h2>
 			<h2 v-else @click="goLogin">点击登录,尊享丝滑</h2>
+			<div class="nav">
+				<el-tooltip content="我的歌单" placement="top" effect="light" :enterable="false">
+					<svg class="icon navIcon" aria-hidden="true">
+						<use xlink:href="#icon-gedan"></use>
+					</svg>
+				</el-tooltip>
+				<el-tooltip content="个人中心" placement="top" effect="light" :enterable="false">
+					<svg class="icon navIcon" aria-hidden="true">
+						<use xlink:href="#icon-gerenzhongxin"></use>
+					</svg>
+				</el-tooltip>
+			</div>
 		</div>
 		<div class="loginFormBox" v-if="show">
 			<div class="formheader">
@@ -25,6 +40,18 @@
 				</el-form-item>
 			</el-form>
 		</div>
+		<div class="popularBoxs">
+			<h2>推荐歌单</h2>
+			<div class="popular">
+				<div class="popularBox" v-for="(item,index) in popularList" :key="index">
+					<div class="songListInfo">
+						<h2>{{item.name}}</h2>
+					</div>
+					<img :src="item.coverImgUrl"/>
+				</div>
+			</div>
+		</div>
+
 	</div>
 </template>
 
@@ -54,9 +81,11 @@
 			return {
 				show: false,
 				Islogin: false,
+				order: 'hot', //new
+				limit: 6,
+				popularList: [],
 				profile: {
 					avatarUrl: '../assets/avatar.jpg'
-
 				},
 				ruleForm: {
 					phone: '15149494081',
@@ -80,6 +109,7 @@
 				this.Islogin = true
 				this.getuserInfo()
 			}
+			this.getpopularList()
 		},
 		methods: {
 			success() {
@@ -94,6 +124,21 @@
 					title: '提示',
 					message: '账号密码错误'
 				});
+			},
+			open4() {
+				this.$message.error('加载歌曲列表失败...');
+			},
+			async getpopularList() {
+				const {
+					data: res
+				} = await this.$axios.get(`/top/playlist?limit=${this.limit}&order=${this.order}`)
+				if (res.code === 200) {
+					this.popularList = res.playlists
+					console.log(this.popularList)
+				} else {
+					this.open4()
+				}
+
 			},
 			submitForm(formName) {
 				this.$refs[formName].validate(async (valid) => {
@@ -120,7 +165,6 @@
 				const userId = window.sessionStorage.getItem('userId')
 				const res = await this.$axios.post(`/user/detail?uid=${userId}`)
 				this.profile = res.data.profile
-				console.log(1)
 			},
 			goLogin() {
 				this.show = true
@@ -139,11 +183,10 @@
 		margin: 0 auto;
 		margin-top: 40px;
 		padding: 40px;
-		// background-color: #fff;
 		text-align: center;
 
 		img {
-			width: 15%;
+			width: 70px;
 			text-align: center;
 			border-radius: 10px;
 		}
@@ -195,5 +238,108 @@
 				flex: 1 0 auto;
 			}
 		}
+	}
+
+	.avatarIcon {
+		font-size: 70px;
+	}
+
+	.navIcon {
+		font-size: 25px;
+		margin: 0 5px;
+	}
+
+	@media screen and (max-width: 500px) {
+		.popularBoxs {
+			width: 100% !important;
+			margin: 0 !important;
+		}
+		.popularBox{
+			width: 100% !important;
+			// height: 100% !important;
+		}
+		.loginFormBox{
+			width: 90% !important;
+		}
+	}
+
+	@media screen and (min-width: 501px) {
+		.popularBoxs {
+			width: 90% !important;
+			margin: 0 !important;
+		}
+		.popularBox{
+			width: 90%;
+			// height: 90% !important;
+		}
+	}
+
+	@media screen and (max-width: 900px) {
+		.popularBoxs {
+			width: 80%;
+		}
+		.popularBox{
+			width: 70%;
+			// height: 70%;
+		}
+	}
+
+	@media screen and (min-width: 901px) {
+		.popularBoxs {
+			width: 80%;
+		}
+		.popularBox{
+			width: 42%;
+		}
+	}
+	.popularBoxs {
+		box-sizing: border-box;
+		margin: 0 auto;
+		padding: 20px;
+		// background-color: #9e9e94;
+	}
+	.popular {
+		display: flex;
+		flex-direction: row;
+		flex-flow: wrap;
+		justify-content: space-evenly;
+	}
+
+	.popularBox {
+		box-sizing: border-box;
+		height: 150px;
+		background-color: #fff;
+		padding: 10px;
+		margin: 20px 20px;
+		border-radius: 20px;
+		transition: all .5s;
+		border: 3px #FFFFFF solid;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.popularBox img {
+		width: 120px;
+		height: 120px;
+		transition: all .5s;
+	}
+	.popularBox:hover {
+		border: 3px #1f4d8f solid;
+		img {
+			transform: translateX(5%);
+		}
+		.songListInfo {
+			transform: translateX(2%);
+		}
+	}
+
+	.popularBoxs h2 {
+		font-size: 30px;
+	}
+	.songListInfo {
+		transition: all .5s;
+	}
+	.songListInfo h2 {
+		font-size: 20px;
 	}
 </style>
